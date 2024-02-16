917f7723eea2b27c3c714a979a805f09abffbb8c
Seth Gran <sethgran9@gmail.com>
2024-02-16 13:24:59 -0500

    ...

--- steam/SteamLobby.gd
+++ steam/SteamLobby.gd
@@ -228,8 +228,7 @@ func process_data(packet_data : Dictionary):
 				if this_member['steam_id'] != Global.STEAM_ID:
 					var player_instance = _player_node.instantiate()
 					player_instance._steam_ID = this_member['steam_id']
-					print(get_tree().get_root().get_node("Floor"))
-					#get_tree().get_root().add_child(player_instance)
+					print(get_tree())
 					player_instance.global_transform.origin = Vector3(1, 0, 1)
 	elif packet_data.has("steam_id"):
 		if packet_data["steam_id"] != 0:
