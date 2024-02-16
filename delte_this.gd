d31a58163c835eca3dd972294380ba2ce26b4d0d
Seth Gran <sethgran9@gmail.com>
2024-02-16 13:40:59 -0500

    ....

--- delte_this.gd
+++ delte_this.gd
@@ -5,7 +5,8 @@ var _player_node = preload("res://scenes/fps_controller.tscn")
 func _ready():
 	for this_member in Global.LOBBY_MEMBERS:
 		if this_member['steam_id'] != Global.STEAM_ID:
+			print("success " + this_member['steam_id'])
 			var player_instance = _player_node.instantiate()
 			player_instance._steam_ID = this_member['steam_id']
 			get_tree().get_root().add_child(player_instance)
-			player_instance.global_transform.origin = Vector3(1, 0, 1)
+			player_instance.global_transform.origin = Vector3(0, 20, 0)
