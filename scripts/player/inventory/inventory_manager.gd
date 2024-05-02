f175e7dbb86056ece890e4e0bcfe8b4730643bf7
Seth Gran <138339656+i-am-set@users.noreply.github.com>
2024-05-01 22:53:26 -0400

    After work push 10:53pm 5/1/2024

--- scripts/player/inventory/inventory_manager.gd
+++ scripts/player/inventory/inventory_manager.gd
@@ -1,20 +1,55 @@
 extends Control
 class_name InventoryManager
 
-@onready var scroll_container = $ScrollContainer
-@onready var v_box_container = $ScrollContainer/VBoxContainer
+const SUBINVENTORY_RECT = preload("res://scenes/ui/subinventory_rect.tscn")
 
-var subinventory_container : Array[SubinventoryManager] = []
+@onready var scroll_container = $ScrollContainer
+@onready var subinventory_container = $ScrollContainer/SubinventoryContainer
+
+var garment_head_id : String = ""
+var garment_face_id : String = ""
+var garment_shirt_id : String = ""
+var garment_subshirt_id : String = ""
+var garment_jacket_id : String = ""
+var garment_pant_id : String = ""
+var garment_subpant_id : String = ""
+var garment_sock_id : String = ""
+var garment_subsock_id : String = ""
+var garment_shoe_id : String = ""
+var garment_back_id : String = ""
+
+var subinventories : Array[SubinventoryManager] = []
 var empty_slot : Dictionary = {"" : 0}
 
 func _ready():
-	pass
+	for child in subinventory_container.get_children():
+		child.queue_free()
+	
+	for i in 3:
+		add_subinventory(randi_range(2, 20))
 
 func try_to_pick_up_item(_picked_up_item_id : String, _stack_size : int) -> bool:
-	for _subinventory : SubinventoryManager in subinventory_container:
-		for i in range(0, 29):
-			if _subinventory.subinventory_contents[i] == empty_slot ||  _subinventory.subinventory_contents[i].has(_picked_up_item_id):
-				_subinventory.add_item(i, _picked_up_item_id, _stack_size)
-				return true
+	var _picked_up_item_size : int = StaticData.item_data[_picked_up_item_id]["item_size"] * _stack_size
+	
+	for _subinventory : SubinventoryManager in subinventories:
+		if _subinventory.subinventory_quantity_held + _picked_up_item_size <= _subinventory.subinventory_capacity:
+			for i in _subinventory.subinventory_slot_amount:
+				if _subinventory.subinventory_contents[i] == empty_slot ||  _subinventory.subinventory_contents[i].has(_picked_up_item_id):
+					_subinventory.add_item(i, _picked_up_item_id, _stack_size)
+					return true
 	
 	return false
+
+func add_subinventory(_subinventory_capacity : int):
+	var _new_subinvnetory : SubinventoryManager = SUBINVENTORY_RECT.instantiate()
+	_new_subinvnetory.subinventory_capacity = _subinventory_capacity
+	subinventory_container.add_child(_new_subinvnetory)
+	subinventories.append(_new_subinvnetory)
+
+func remove_subinventory(_subinventory_to_remove : SubinventoryManager):
+	if subinventories.has(_subinventory_to_remove):
+		subinventory_container.remove_child(_subinventory_to_remove)
+		subinventories.erase(_subinventory_to_remove)
+
+func update_equipment():
+	pass
--- scripts/player/inventory/subinventory_manager.gd
+++ scripts/player/inventory/subinventory_manager.gd
@@ -5,37 +5,50 @@ const INV_ITEM_RECT = preload("res://scenes/ui/inv_item_rect.tscn")
 
 @onready var inventory_manager: InventoryManager = $"../../.."
 @onready var header_button: TextureButton = $HeaderButton
+@onready var items_container = $ItemsContainer
+@onready var subinventory_name_label = $HeaderButton/Label
+@onready var capacity_label = $HeaderButton/CapacityLabel
+
+@export var capacity_color_normal: Color = Color(1, 1, 1)
+@export var capacity_color_almost_full: Color = Color(1, 0.5, 0.5)
+@export var capacity_color_full: Color = Color(1, 0, 0)
+@export var full_capacity_threshhold : float = 0.8
+@export var subinventory_capacity : int = 10
+var subinventory_quantity_held : int = 0
+var subinventory_slot_amount : int = subinventory_capacity + 1
 
 var subinventory_contents : Array[Dictionary] = []
 var empty_slot : Dictionary = {"" : 0}
 var item_rects : Array[Panel] = []
 
 func _ready() -> void:
-	inventory_manager.subinventory_container.append(self)
+	await initialize_subinventory()
+
+func initialize_subinventory():
 	await initialize_subinventory_slots()
 	await initialize_item_rects()
 	await update_subinventory()
+	subinventory_name_label.text = name
 
 func initialize_subinventory_slots():
-	for child in get_children():
-		if child != header_button:
-			child.queue_free()
+	for child in items_container.get_children():
+		child.queue_free()
 	
 	await get_tree().create_timer(0.5).timeout
 	
-	for i in range(1, 30):
+	for i in subinventory_slot_amount:
 		subinventory_contents.append(empty_slot)
 		var _new_inv_item_rect = INV_ITEM_RECT.instantiate()
 		_new_inv_item_rect.hide()
-		add_child(_new_inv_item_rect)
+		items_container.add_child(_new_inv_item_rect)
 
 func initialize_item_rects():
-	for child in get_children():
-		if child is Panel:
+	for child in items_container.get_children():
+		if child is InvItemRect:
 			item_rects.append(child)
 
 func update_item_rects():
-	for i in subinventory_contents.size():
+	for i in subinventory_slot_amount:
 		var _item_in_slot = subinventory_contents[i]
 		if _item_in_slot != empty_slot:
 			var _item_rect = item_rects[i]
@@ -46,13 +59,24 @@ func update_item_rects():
 			item_rects[i].hide()
 
 func update_subinventory():
-	if check_if_subinventory_empty():
-		hide()
+	#var is_empty : bool = check_if_subinventory_empty()
+	#if is_empty && items_container.visible:
+		#items_container.hide()
+	#elif !is_empty && !items_container.visible:
+		#items_container.show()
+	
+	capacity_label.text = "%d/%d" %[subinventory_quantity_held, subinventory_capacity]
+	
+	var capacity_used_percentage : float = float(subinventory_quantity_held) / float(subinventory_capacity)
+	if capacity_used_percentage >= 1.0:
+		capacity_label.modulate = capacity_color_full
+	elif capacity_used_percentage >= full_capacity_threshhold:
+		capacity_label.modulate = capacity_color_almost_full
 	else:
-		show()
+		capacity_label.modulate = capacity_color_normal
 
 func check_if_subinventory_empty() -> bool:
-	for i in subinventory_contents.size():
+	for i in subinventory_slot_amount:
 		if subinventory_contents[i] != empty_slot:
 			return false
 	
@@ -60,12 +84,18 @@ func check_if_subinventory_empty() -> bool:
 
 func add_item(_item_slot : int, _picked_up_item_id : String, _stack_size : int):
 	var _new_item : Dictionary = {_picked_up_item_id : _stack_size}
+	var _picked_up_item_size : int = StaticData.item_data[_picked_up_item_id]["item_size"] * _stack_size
 	
 	if subinventory_contents[_item_slot] == empty_slot:
 		subinventory_contents[_item_slot] = _new_item
 	elif subinventory_contents[_item_slot].has(_picked_up_item_id):
 		var _cached_item_stack_size = subinventory_contents[_item_slot][_picked_up_item_id]
 		subinventory_contents[_item_slot][_picked_up_item_id] = _cached_item_stack_size + _stack_size
+	else:
+		printerr("Item slot is already occupied. Item slot ", _item_slot, " in ", name, ".")
+		return
+	
+	subinventory_quantity_held += _picked_up_item_size
 	
 	update_item_rects()
 	update_subinventory()
