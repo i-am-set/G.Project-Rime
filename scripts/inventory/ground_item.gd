2346325756e3cbc215935a88da1309d3d65a1930
Seth Gran <sethgran9@gmail.com>
2024-07-06 14:09:43 -0400

    ...

--- scripts/inventory/ground_item.gd
+++ scripts/inventory/ground_item.gd
@@ -2,11 +2,7 @@ extends RigidBody3D
 
 const HIGHLIGHT_MATERIAL = preload("res://materials/utility/highlight_material.tres")
 
-@onready var mesh_instance = $MeshInstance3D
-@onready var collision_shape = $CollisionShape3D
 @onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
-@onready var area_3d = $Area3D
-@onready var repulsion_area = $RepulsionArea
 
 const ITEM_ICONS : Dictionary = {
 	"a000001" : preload("res://textures/items/icons/ico_flint.png"),
@@ -71,12 +67,7 @@ func randomize_scale():
 	var random_scale = randf_range(0.75, 1.25)
 	scale = Vector3(random_scale, random_scale, random_scale)
 
-func _on_body_entered(body):
+func _on_body_entered(body: Node) -> void:
 	if body.collision_layer == 1:
 		await get_tree().create_timer(0.5).timeout
 		freeze = true
-
-func _on_repulsion_area_area_entered(area):
-	var diff : Vector3 = global_position - area.global_position
-	var new_velocity = diff.normalized()
-	apply_central_force(Vector3(10,0,0))
