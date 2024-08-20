204d9b69913bd6552528fc2b868d2f6ddb2a2cb7
Seth Gran <sethgran9@gmail.com>
2024-08-20 10:21:16 -0400

    ...

--- .godot/mono/temp/obj/Debug/Godot FPS Project.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/Godot FPS Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Godot FPS Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934a696e1c0e27e35fb78341402a3da93c2bb4d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Godot FPS Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Godot FPS Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
