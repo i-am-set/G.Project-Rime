b75c78ff1a74916bc6e5770e3924c22321bc59fd
Seth Gran <sethgran9@gmail.com>
2024-11-13 16:04:35 -0500

    ...

--- data/StaticData.gd
+++ data/StaticData.gd
@@ -1,12 +1,19 @@
 extends Node
 
 var item_data
+var recipe_data
+var structure_data
 var item_data_file_path = "res://data/Item_Data.json"
+var recipe_data_file_path = "res://data/Recipe_Data.json"
+var structure_data_file_path = "res://data/Structure_Data.json"
+
 
 const MISSING_MESH_MODEL = preload("res://meshes/utility/missing_mesh_model.obj")
 
 func _ready():
 	item_data = import_json_file(item_data_file_path)
+	recipe_data = import_json_file(recipe_data_file_path)
+	structure_data_file_path = import_json_file(structure_data_file_path)
 	await get_tree().create_timer(0.25).timeout
 	
 	Console.create_command("get_item_list", self.c_list_all_items, "Lists all items and their IDs.")
